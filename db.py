81e766faf9d674916ae7730f7fa1e2b6ba1d4991
Hedi Belkahia <hedibelkahia03@gmail.com>
2025-02-25 03:16:38 -0500

    Add functionality to prevent duplicate crypto entries and implement price update alerts

--- db.py
+++ db.py
@@ -8,10 +8,14 @@ def create_table():
     cur.execute("CREATE TABLE IF NOT EXISTS cryptos (ticker TEXT PRIMARY KEY, name TEXT, price REAL, last_update TEXT)")
 
 def add_crypto(ticker, name):
+    if get_crypto(ticker) is not None:
+        return None
     cur.execute("INSERT INTO cryptos (ticker, name, price, last_update) VALUES (?, ?, ?, ?)", (ticker, name, -1, "never"))
     con.commit()
 
 def update_crypto(ticker, price):
+    if get_crypto(ticker) is None:
+        return None
     cur.execute("UPDATE cryptos SET price = ?, last_update = datetime('now') WHERE ticker = ?", (price, ticker))
     con.commit()
 
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import smtplib
 from email.message import EmailMessage
 import requests
-import time
 import os
 from dotenv import load_dotenv
 from db import add_crypto, update_crypto, get_all, get_crypto
 
 
 
-# email config
+# config
 load_dotenv()
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 TO_EMAIL = os.getenv('TO_EMAIL') # could be same as EMAIL_ADDRESS
 
 API_KEY = os.getenv('API_KEY')
+
+PERCENT_THRESHOLD = 2.0
 
 
 def send_email(subject, body):
@@ -29,6 +30,7 @@ def send_email(subject, body):
         connection.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
         connection.send_message(msg)
 
+    print("Email sent.")
 
 
 def get_price(symbol):
@@ -42,8 +44,29 @@ def get_price(symbol):
     return response.json()[symbol]["usd"]
 
 
+def update_all():
+    db_cryptos = get_all()
+    symbols = ",".join([crypto[1] for crypto in db_cryptos])
+    tickers = [crypto[0] for crypto in db_cryptos]
+    old_prices = [crypto[2] for crypto in db_cryptos]
+
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={symbols}&vs_currencies=usd"
+    headers = {
+        "accept": "application/json",
+        "x-cg-demo-api-key": API_KEY
+    }
+    response = requests.get(url, headers=headers)
+
+    for ticker in tickers:
+        price = response.json()[get_crypto(ticker)[1]]["usd"]
+        update_crypto(ticker, price)
+
+        if price > old_prices[tickers.index(ticker)] * (1 + PERCENT_THRESHOLD / 100):
+            send_email(f"{ticker} Alert", f"{ticker} has increased by more than {PERCENT_THRESHOLD}% in the last hour.")
+
+
+
+
 if __name__ == "__main__":
-    print(get_all())
-    add_crypto("SOL", "solana")
-    update_crypto("SOL", get_price("solana"))
-    print(get_crypto("SOL"))
+    update_all()
+    #print(get_all())
